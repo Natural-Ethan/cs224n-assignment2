8c44f18c022fcae5bcaa3b8e67ac793f0cbf5aff
Leo WU <leowu@LeodeMacBook-Pro.local>
2022-01-09 21:04:19 +0800

    run: change the iter from 40,000 to 4,000; sgd: invert the - of iteration to +

--- sgd.py
+++ sgd.py
@@ -87,11 +87,11 @@ def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
         loss = None
         ### YOUR CODE HERE (~2 lines)
         loss, grad = f(x)
-        x = x - step * grad 
+        x = x + step * grad 
 
         # if iter % PRINT_EVERY == 0:
         #     print("loss = %f " % loss)
-        
+
         ### END YOUR CODE
 
         x = postprocessing(x)
